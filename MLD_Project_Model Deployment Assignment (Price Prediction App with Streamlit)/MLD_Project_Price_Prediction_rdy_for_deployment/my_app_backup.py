4df9e77c8e1b5dc414c862d2269736c1bc61d712
Robin Alexander Riemel <r.riemel@gmail.com>
2023-08-31 17:49:07 +0200

    small adjustments

--- MLD_Project_Model Deployment Assignment (Price Prediction App with Streamlit)/MLD_Project_Price_Prediction_rdy_for_deployment/my_app_backup.py
+++ MLD_Project_Model Deployment Assignment (Price Prediction App with Streamlit)/MLD_Project_Price_Prediction_rdy_for_deployment/my_app_backup.py
@@ -6,7 +6,6 @@ from PIL import Image  # to deal with images (PIL: Python imaging library)
 
 # Title/Text
 st.title("This is a the price prediction app!")
-st.header("Please fill out the form elements in the sidebar and press the predict Button to get the price prediction")
 
 ## Streamlit for Price Prediction App
 
@@ -15,16 +14,14 @@ import pickle
 filename = "my_final_model"
 model=pickle.load(open(filename, "rb"))
 
-# Car Model Selection
-st.sidebar.subheader("Car Details")
+# To take feature inputs
+#make_model = st.sidebar.number_input("Model:",min_value=5, max_value=300)
 make_model = st.sidebar.selectbox("Model:", ["Audi A1", "Audi A3", "Opel Astra", "Opel Insignia", "Opel Corsa", "Renault Clio", "Renault Espace" ])
-
-## Numeric Inputs
-hp_kW = st.sidebar.slider("Horsepower:",min_value=20, max_value=300)
-km = st.sidebar.slider("Kilometer:",min_value=0, max_value=400000, step=1000)
+hp_kW = st.sidebar.number_input("Horsepower:",min_value=40, max_value=300)
+km = st.sidebar.number_input("Kilometer:",min_value=0, max_value=350000)
 age = st.sidebar.number_input("Age:",min_value=0, max_value=3)
 Comfort_Convenience = st.sidebar.number_input("Comfort Features:",min_value=0, max_value=20)
-Displacement_cc = st.sidebar.slider("CC (Displacement in cubic cm)::",min_value=500, max_value=3500, , step=100)
+Displacement_cc = st.sidebar.number_input("CC:",min_value=500, max_value=3500)
 
 
 # Create a dataframe using feature inputs
